d874e67a4d3da77980691aab4f857227c3e5b163
Lewis Collum <lewiscollum4@gmail.com>
2018-11-19 15:34:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lewis/sensorlogger/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lewis.sensorlogger;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.os.Bundle;
+import android.os.Environment;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    private SensorRecorder accelerometer;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        accelerometer = new SensorRecorder(this, Sensor.TYPE_LINEAR_ACCELERATION);
+        //File root = Environment.getExternalStorageDirectory();
+        //if(root.canWrite()){
+        //    File dir = new File(root.getAbsolutePath() + "SensorLogger");
+        //    dir.mkdirs();
+        //}
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        accelerometer.stopRecording();
+    }
+}
